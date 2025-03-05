e48e75c47e8ff6853a5fa30282639da401809f98
KinglyMinecon <88132731+KinglyMinecon@users.noreply.github.com>
2025-03-05 10:45:43 +0000

    Documentation edits made through Mintlify web editor

--- public/crisp.js
+++ public/crisp.js
@@ -1,12 +1,1 @@
-if (typeof window !== "undefined") {
-  window.$crisp = [];
-  window.CRISP_WEBSITE_ID = "19ea015a-f7b7-46aa-939a-c73c31083509";
-
-  (function () {
-    const d = document;
-    const s = d.createElement("script");
-    s.src = "https://client.crisp.chat/l.js";
-    s.async = true;
-    d.getElementsByTagName("head")[0].appendChild(s);
-  })();
-}
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="19ea015a-f7b7-46aa-939a-c73c31083509";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
